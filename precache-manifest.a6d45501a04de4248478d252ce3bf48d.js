1eeed77218490f73f32db8fc1c0693d929e2a805
Felipe Agustini <felipeboccardi@hotmail.com>
2020-05-28 16:37:56 -0300

    Updates

---
+++ precache-manifest.a6d45501a04de4248478d252ce3bf48d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a5a351cb1bf0ceb7786f9bac36b53a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6997a9932e35ba17273a",
+    "url": "/portfolio/static/css/main.bfb7829d.chunk.css"
+  },
+  {
+    "revision": "eb30677317099e626120",
+    "url": "/portfolio/static/js/2.5ad11bd9.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/portfolio/static/js/2.5ad11bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6997a9932e35ba17273a",
+    "url": "/portfolio/static/js/main.76fa2679.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8397d785470d054f3bf1200b1e486339",
+    "url": "/portfolio/static/media/codebuddy_br.8397d785.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "4403f4b9300ed4b72a6409f4cd7f69be",
+    "url": "/portfolio/static/media/developerActivity.4403f4b9.svg"
+  },
+  {
+    "revision": "082fc9c797668a60dda0fd26eda02408",
+    "url": "/portfolio/static/media/manOnTable.082fc9c7.svg"
+  },
+  {
+    "revision": "600f3832f7d7483995802b957b3cf3a2",
+    "url": "/portfolio/static/media/skill.600f3832.svg"
+  }
+]);
